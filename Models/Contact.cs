4af629aa9c30a6fe15f5d38b1276a7aacce2a3e6
Brady Gaster <bradyg@microsoft.com>
2015-11-19 22:33:42 -0800

    initial create

---
+++ Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ContactList.Models
+{
+    public class Contact
+    {
+        /// <summary>
+        /// The ID of the contact.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The Contact's full name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The Contact's email address. 
+        /// </summary>
+        public string EmailAddress { get; set; }
+    }
+}
